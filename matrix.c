aeae5766e684ea6cab56a2a12bfebad6b451d97c
Juliana Felix <juhffelix@outlook.com>
2024-01-21 19:29:39 -0300

    matriz

---
+++ matrix.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+   
+    int matriz[3][3];
+
+    
+    printf("Digite os elementos da matriz 3x3:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("Elemento [%d][%d]: ", i, j);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            matriz[i][j] *= 5;
+        }
+    }
+
+   
+    printf("\nMatriz multiplicada por cinco:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
